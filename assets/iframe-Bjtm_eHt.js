b6a0b8af75765dc1fd61e5d1683e5c923e6a72d8
bortoz <bortoz@users.noreply.github.com>
2024-09-05 11:36:08 +0000

    deploy: 5548dc2b716cca38b3c6d92af05bdb3f36dc9066

--- assets/iframe-Bjtm_eHt.js
+++
@@ -1,4 +0,0 @@
-const __vite__fileDeps=["assets/editor-B6APS23L-Dw_KzdCS.js","assets/commonjs-helper-3Jsk8tIi.js","assets/_baseIsEqual-CCXSSeP_.js","assets/isArray-BCUZC26q.js","assets/editor-B6APS23L-1yVlOmYN.css"],__vite__mapDeps=i=>i.map(i=>__vite__fileDeps[i]);
-/*! For licenses information, see LICENSES.txt */
-import{_}from"./preload-helper-B6uM3qOM.js";_(()=>import("./editor-B6APS23L-Dw_KzdCS.js"),__vite__mapDeps([0,1,2,3,4]));
-//# sourceMappingURL=iframe-Bjtm_eHt.js.map
