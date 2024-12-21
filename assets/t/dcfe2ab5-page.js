d520f08849755be4b09e7ba64c89957a9ae5400d
Harniver <Harniver@users.noreply.github.com>
2024-12-21 11:51:30 +0000

    deploy: 548ec58de0cdedcea39312619638fc655435a375

---
+++ assets/t/dcfe2ab5-page.js
@@ -0,0 +1,26 @@
+/*! For licenses information, see LICENSES.txt */
+import{j as e}from"../_virtual_quizms-routes-e5ffd91c.js";import{n as s,f}from"../w/c06ec230-chunk-IFWY6GUU.js";import"../s/00dd80a9-react-dom.js";import{M as g,a as S,b as C,c as D,d as P,e as b,f as M,g as v}from"../o/28b451e6-question.js";function j(){return{...s(),P1CicliCondizioni:g,P2BalleDiFieno:S,P3PileDiCarote:C,P4Contatore:D,S1OrdinaPalloni:P,S2GaraSalto:b,S3SquadreBasket:M,S4TorriGemelle:v}}function u(i){const n={h2:"h2",...j(),...i.components},{Contest:a,P2BalleDiFieno:c,P3PileDiCarote:l,P4Contatore:d,Problem:t,S2GaraSalto:h,S3SquadreBasket:p,S4TorriGemelle:m,Section:r}=n;return a||o("Contest"),c||o("P2BalleDiFieno"),l||o("P3PileDiCarote"),d||o("P4Contatore"),t||o("Problem"),h||o("S2GaraSalto"),p||o("S3SquadreBasket"),m||o("S4TorriGemelle"),r||o("Section"),e.jsxs(a,{children:[e.jsx(n.h2,{children:"Sezione 1: procedimenti procedurali"}),e.jsxs(r,{children:[e.jsx(t,{points:[5,1,0],id:1,children:e.jsx(c,{})}),e.jsx(t,{points:[5,1,0],id:2,children:e.jsx(l,{})}),e.jsx(t,{points:[5,1,0],id:3,children:e.jsx(d,{})})]}),e.jsx(n.h2,{children:"Sezione 2: programmazione"}),e.jsxs(r,{children:[e.jsx(t,{points:[5,0,0],id:4,children:e.jsx(h,{})}),e.jsx(t,{points:[5,0,0],id:5,children:e.jsx(p,{})}),e.jsx(t,{points:[5,0,0],id:6,children:e.jsx(m,{})})]})]})}function X(i={}){const{wrapper:n}={...j(),...i.components};return n?e.jsx(n,{...i,children:e.jsx(u,{...i})}):u(i)}function o(i,n){throw new Error("Expected component `"+i+"` to be defined: you likely forgot to import, pass, or provide it.")}function x(i){const n={em:"em",h1:"h1",h2:"h2",li:"li",p:"p",strong:"strong",ul:"ul",...s(),...i.components};return e.jsxs(e.Fragment,{children:[e.jsx(n.h1,{children:"Giochi di Fibonacci 2023/2024"}),`
+`,e.jsx(n.h2,{children:"Fase II - Scuole secondarie"}),`
+`,e.jsxs(n.p,{children:["Questa prova contiene ",e.jsx(n.em,{children:"6 domande"})," da risolvere in ",e.jsx(n.em,{children:"100 minuti"}),`.
+Le domande sono a `,e.jsx(n.strong,{children:"scelta multipla"})," o a ",e.jsx(n.strong,{children:"blocchi"}),", e sono divise in due parti:"]}),`
+`,e.jsxs(n.ul,{children:[`
+`,e.jsxs(n.li,{children:["domande di interpretazione di ",e.jsx(n.strong,{children:"procedimenti procedurali"})," come programmi a blocchi, e"]}),`
+`,e.jsxs(n.li,{children:["domande di ",e.jsx(n.strong,{children:"programmazione"})," tramite blocchi."]}),`
+`]}),`
+`,e.jsxs(n.p,{children:[`In entrambe le parti, le domande sono ordinate per difficoltà crescente.
+`,e.jsx(n.strong,{children:"Attento che la difficoltà è soggettiva!"})," Se stai passando tanto tempo cercando di risolvere una domanda, prova a passare ad altre domande e altre categorie!"]}),`
+`,e.jsx(n.h2,{children:"Punteggio"}),`
+`,e.jsxs(n.p,{children:["Tutte le domande a ",e.jsx(n.em,{children:"scelta multipla"})," hanno 5 opzioni, di cui ",e.jsx(n.strong,{children:"solo una"})," è corretta. Il punteggio che puoi ottenere è:"]}),`
+`,e.jsxs(n.ul,{children:[`
+`,e.jsxs(n.li,{children:["5 punti per una risposta ",e.jsx(n.em,{children:"corretta"}),";"]}),`
+`,e.jsxs(n.li,{children:["1 punto per una risposta ",e.jsx(n.em,{children:"non data"}),";"]}),`
+`,e.jsxs(n.li,{children:["0 punti per una risposta ",e.jsx(n.em,{children:"sbagliata"}),"."]}),`
+`]}),`
+`,e.jsxs(n.p,{children:["Le domande a ",e.jsx(n.em,{children:"blocchi"}),` richiedono di scrivere un singolo programma a blocchi, che viene valutato su tre diversi livelli.
+Per ciascuna domanda e per ciascun livello, Il punteggio che puoi ottenere è:`]}),`
+`,e.jsxs(n.ul,{children:[`
+`,e.jsxs(n.li,{children:["5 punti se il programma produce la risposta ",e.jsx(n.em,{children:"corretta"}),";"]}),`
+`,e.jsxs(n.li,{children:["0 punti se il programma produce una risposta ",e.jsx(n.em,{children:"sbagliata"}),"."]}),`
+`]}),`
+`,e.jsx(n.p,{children:"Quindi ogni domanda a blocchi può valere fino a 15 punti in totale."})]})}function F(i={}){const{wrapper:n}={...s(),...i.components};return n?e.jsx(n,{...i,children:e.jsx(x,{...i})}):x(i)}const B={title:"Giochi di Fibonacci 2023/2024 - Fase II - Scuole secondarie",description:"Seconda fase dei Giochi di Fibonacci 2023/2024 per le scuole secondarie"};function _(){return e.jsxs(f,{contestName:"Giochi di Fibonacci",duration:100,children:[e.jsx(F,{}),e.jsx(X,{})]})}export{_ as default,B as metadata};
+//# sourceMappingURL=dcfe2ab5-page.js.map
