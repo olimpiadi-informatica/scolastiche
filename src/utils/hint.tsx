76e5a684e6d02f2b95aaa15c49093cd64bd38eec
Giorgio Audrito <giorgio.audrito@gmail.com>
2025-05-25 15:06:56 +0200

    finita lezione 1

--- src/utils/hint.tsx
+++ src/utils/hint.tsx
@@ -3,15 +3,15 @@ import { type ReactNode, useRef } from "react";
 import { Button, Modal } from "@olinfo/react-components";
 
 export function Hint({
-  label = "Mostra suggerimento",
+  label = "suggerimento",
   children,
 }: { label: string; children: ReactNode }) {
   const ref = useRef<HTMLDialogElement>(null);
 
   return (
     <div className="w-full flex justify-center my-2">
-      <Button onClick={() => ref.current?.showModal()}>{label}</Button>
-      <Modal ref={ref} title="Suggerimento">
+      <Button onClick={() => ref.current?.showModal()}>Mostra {label}</Button>
+      <Modal ref={ref} title={label.charAt(0).toUpperCase() + label.slice(1)}>
         {children}
       </Modal>
     </div>
