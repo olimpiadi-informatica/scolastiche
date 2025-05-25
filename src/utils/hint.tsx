3087fb0b9ba7052b4d6ea2808938d8d380f1a42e
Alessandro Bortolin <bortolin.alessandro@outlook.it>
2025-05-25 14:59:03 +0200

    Hint migliorati

--- src/utils/hint.tsx
+++ src/utils/hint.tsx
@@ -2,12 +2,15 @@ import { type ReactNode, useRef } from "react";
 
 import { Button, Modal } from "@olinfo/react-components";
 
-export function Hint({ children }: { children: ReactNode }) {
+export function Hint({
+  label = "Mostra suggerimento",
+  children,
+}: { label: string; children: ReactNode }) {
   const ref = useRef<HTMLDialogElement>(null);
 
   return (
-    <div className="w-full flex justify-center">
-      <Button onClick={() => ref.current?.showModal()}>Mostra suggerimento</Button>
+    <div className="w-full flex justify-center my-2">
+      <Button onClick={() => ref.current?.showModal()}>{label}</Button>
       <Modal ref={ref} title="Suggerimento">
         {children}
       </Modal>
