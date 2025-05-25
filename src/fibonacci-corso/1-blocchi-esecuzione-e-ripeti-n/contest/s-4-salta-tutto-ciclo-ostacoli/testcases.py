ebf99e7311827cec904582ec490e0a1ccb6c2243
Giorgio Audrito <giorgio.audrito@gmail.com>
2025-05-25 14:50:12 +0200

    esercizio 4

---
+++ src/fibonacci-corso/1-blocchi-esecuzione-e-ripeti-n/contest/s-4-salta-tutto-ciclo-ostacoli/testcases.py
@@ -0,0 +1,13 @@
+import random
+import json
+random.seed(0)
+testcases = []
+RS = [
+    ([0, 1, 0], 2),
+    ([0, 1, 0, 1, 0, 1, 0], 6),
+    ([0, 0, 1, 0, 1, 0, 0, 0], 5),
+    ([0, 1, 0, 1, 0, 0, 0, 1, 0, 0], 9),
+]
+for R,n in RS:
+    testcases.append({"rocks": R, "pos": 0, "flight": 0, "N": n, "M": sum(R)})
+print(json.dumps(list(testcases)))
