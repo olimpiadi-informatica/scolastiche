00258d56ce5b28b2e95e9b875863e900e4bd21fd
bortoz <bortoz@users.noreply.github.com>
2025-05-25 12:50:31 +0000

    deploy: 00cbb6b44d6d0d308f198c862ad5a3537739590f

---
+++ assets/t/e7e137f5-page.js
@@ -0,0 +1,20 @@
+/*! For licenses information, see LICENSES.txt */
+import{j as e}from"../_virtual_quizms-routes-86c6950b.js";import{P as x,I as g}from"../u/6cb5ddaf-components.js";import{M as C,a as b,b as P,c as A,d as D,e as M,f as X}from"../o/31f53a1b-question.js";function j(){return{...x(),A11ConnettiStati:X,P6DistribuisciCarote:M,A4SottraiPotenze:D,A20DueCarote:A,A21OrdinaConigli:P,A22PesaLaFrutta:b,P5PercorsoAdOstacoli:C}}function u(i){const n={h2:"h2",...j(),...i.components},{A11ConnettiStati:s,A21OrdinaConigli:a,A22PesaLaFrutta:c,A4SottraiPotenze:d,Contest:l,P5PercorsoAdOstacoli:p,P6DistribuisciCarote:m,Problem:o,Section:r}=n;return s||t("A11ConnettiStati"),a||t("A21OrdinaConigli"),c||t("A22PesaLaFrutta"),d||t("A4SottraiPotenze"),l||t("Contest"),p||t("P5PercorsoAdOstacoli"),m||t("P6DistribuisciCarote"),o||t("Problem"),r||t("Section"),e.jsxs(l,{children:[e.jsx(n.h2,{children:"Sezione 1: pensiero logico-algoritmico"}),e.jsxs(r,{children:[e.jsx(o,{points:[5,1,0],id:1,children:e.jsx(a,{})}),e.jsx(o,{points:[5,1,0],id:2,children:e.jsx(c,{})}),e.jsx(o,{points:[5,1,0],id:3,children:e.jsx(d,{})}),e.jsx(o,{points:[5,1,0],id:4,children:e.jsx(s,{})})]}),e.jsx(n.h2,{children:"Sezione 2: procedimenti procedurali"}),e.jsxs(r,{children:[e.jsx(o,{points:[5,1,0],id:5,children:e.jsx(m,{})}),e.jsx(o,{points:[5,1,0],id:6,children:e.jsx(p,{})})]})]})}function S(i={}){const{wrapper:n}={...j(),...i.components};return n?e.jsx(n,{...i,children:e.jsx(u,{...i})}):u(i)}function t(i,n){throw new Error("Expected component `"+i+"` to be defined: you likely forgot to import, pass, or provide it.")}function f(){return{...x()}}function h(i){const n={em:"em",h1:"h1",h2:"h2",li:"li",p:"p",strong:"strong",ul:"ul",...f(),...i.components};return e.jsxs(e.Fragment,{children:[e.jsx(n.h1,{children:"Giochi di Fibonacci 2024/2025"}),`
+`,e.jsx(n.h2,{children:"Fase I - Scuole secondarie"}),`
+`,e.jsxs(n.p,{children:["Questa prova contiene ",e.jsx(n.em,{children:"6 problemi"})," da risolvere in ",e.jsx(n.em,{children:"50 minuti"}),", ed è divisa in due parti:"]}),`
+`,e.jsxs(n.ul,{children:[`
+`,e.jsxs(n.li,{children:["quattro problemi di ",e.jsx(n.strong,{children:"pensiero logico-algoritmico"}),", e"]}),`
+`,e.jsxs(n.li,{children:["due problemi di interpretazione di ",e.jsx(n.strong,{children:"procedimenti procedurali"})," come programmi a blocchi."]}),`
+`]}),`
+`,e.jsxs(n.p,{children:[`In entrambe le parti i problemi sono ordinati per difficoltà crescente.
+`,e.jsx(n.strong,{children:"Attento che la difficoltà è soggettiva!"})," Se stai passando tanto tempo cercando di risolvere un problema, prova a passare ad altri problemi e altre categorie!"]}),`
+`,e.jsx(n.h2,{children:"Punteggio"}),`
+`,e.jsxs(n.p,{children:["Ogni problema comprende ",e.jsx(n.em,{children:"due domande"}),`, valutate separatamente.
+La prima domanda è `,e.jsx(n.strong,{children:"sempre più semplice"}),`, e può aiutare a rispondere alla seconda domanda.
+Le domande sono tutte a `,e.jsx(n.strong,{children:"scelta multipla"})," con 5 opzioni, di cui ",e.jsx(n.strong,{children:"solo una"})," è corretta. Il punteggio che puoi ottenere è:"]}),`
+`,e.jsxs(n.ul,{children:[`
+`,e.jsxs(n.li,{children:["5 punti per una risposta ",e.jsx(n.em,{children:"corretta"}),";"]}),`
+`,e.jsxs(n.li,{children:["1 punto per una risposta ",e.jsx(n.em,{children:"non data"}),";"]}),`
+`,e.jsxs(n.li,{children:["0 punti per una risposta ",e.jsx(n.em,{children:"sbagliata"}),"."]}),`
+`]})]})}function $(i={}){const{wrapper:n}={...f(),...i.components};return n?e.jsx(n,{...i,children:e.jsx(h,{...i})}):h(i)}const z={title:"Giochi di Fibonacci",description:"Prima fase dei Giochi di Fibonacci 2024/2025 per le scuole secondarie"};function O(){return e.jsxs(g,{contestLongName:"",contestName:"Giochi di Fibonacci",duration:50,children:[e.jsx($,{}),e.jsx(S,{})]})}export{O as default,z as metadata};
+//# sourceMappingURL=e7e137f5-page.js.map
