b46847a21b003a7daf263d917de68f7282d4160c
Harniver <Harniver@users.noreply.github.com>
2025-05-26 20:34:04 +0000

    deploy: c85c74bbcbdc0394479b74a0b709ba343e7c4018

---
+++ assets/t/aaab41a0-page.js
@@ -0,0 +1,26 @@
+/*! For licenses information, see LICENSES.txt */
+import{j as n}from"../_virtual_quizms-routes-f8bd54d5.js";import{P as x,I as f}from"../u/d725959b-components.js";import{M,a as C,b,c as v,d as P,e as S}from"../o/ea80a92e-question.js";function j(){return{...x(),P2Condizioni:S,P3Mediana:P,P4CicloLista:v,S2AlzaTorre:b,S4Muro:C,S3GiraRuota:M}}function u(i){const e={h2:"h2",...j(),...i.components},{Contest:s,P2Condizioni:a,P3Mediana:c,P4CicloLista:d,Problem:r,S2AlzaTorre:l,S3GiraRuota:h,S4Muro:p,Section:t}=e;return s||o("Contest"),a||o("P2Condizioni"),c||o("P3Mediana"),d||o("P4CicloLista"),r||o("Problem"),l||o("S2AlzaTorre"),h||o("S3GiraRuota"),p||o("S4Muro"),t||o("Section"),n.jsxs(s,{children:[n.jsx(e.h2,{children:"Sezione 1: procedimenti procedurali"}),n.jsxs(t,{children:[n.jsx(r,{points:[5,1,0],id:1,children:n.jsx(a,{})}),n.jsx(r,{points:[5,1,0],id:2,children:n.jsx(c,{})}),n.jsx(r,{points:[5,1,0],id:3,children:n.jsx(d,{})})]}),n.jsx(e.h2,{children:"Sezione 2: programmazione"}),n.jsxs(t,{children:[n.jsx(r,{points:[5,0,0],id:4,children:n.jsx(l,{})}),n.jsx(r,{points:[5,0,0],id:5,children:n.jsx(h,{})}),n.jsx(r,{points:[5,0,0],id:6,children:n.jsx(p,{})})]})]})}function z(i={}){const{wrapper:e}={...j(),...i.components};return e?n.jsx(e,{...i,children:n.jsx(u,{...i})}):u(i)}function o(i,e){throw new Error("Expected component `"+i+"` to be defined: you likely forgot to import, pass, or provide it.")}function g(){return{...x()}}function m(i){const e={em:"em",h1:"h1",h2:"h2",li:"li",p:"p",strong:"strong",ul:"ul",...g(),...i.components};return n.jsxs(n.Fragment,{children:[n.jsx(e.h1,{children:"Giochi di Fibonacci 2023/2024"}),`
+`,n.jsx(e.h2,{children:"Allenamento per la Fase II - Scuole secondarie"}),`
+`,n.jsxs(e.p,{children:["Questa prova contiene ",n.jsx(e.em,{children:"6 domande"})," da risolvere in ",n.jsx(e.em,{children:"100 minuti"}),`.
+Le domande sono a `,n.jsx(e.strong,{children:"scelta multipla"})," o a ",n.jsx(e.strong,{children:"blocchi"}),", e sono divise in due parti:"]}),`
+`,n.jsxs(e.ul,{children:[`
+`,n.jsxs(e.li,{children:["domande di interpretazione di ",n.jsx(e.strong,{children:"procedimenti procedurali"})," come programmi a blocchi, e"]}),`
+`,n.jsxs(e.li,{children:["domande di ",n.jsx(e.strong,{children:"programmazione"})," tramite blocchi."]}),`
+`]}),`
+`,n.jsxs(e.p,{children:[`In entrambe le parti, le domande sono ordinate per difficoltà crescente.
+`,n.jsx(e.strong,{children:"Attento che la difficoltà è soggettiva!"})," Se stai passando tanto tempo cercando di risolvere una domanda, prova a passare ad altre domande e altre categorie!"]}),`
+`,n.jsx(e.h2,{children:"Punteggio"}),`
+`,n.jsxs(e.p,{children:["Tutte le domande a ",n.jsx(e.em,{children:"scelta multipla"})," hanno 5 opzioni, di cui ",n.jsx(e.strong,{children:"solo una"})," è corretta. Il punteggio che puoi ottenere è:"]}),`
+`,n.jsxs(e.ul,{children:[`
+`,n.jsxs(e.li,{children:["5 punti per una risposta ",n.jsx(e.em,{children:"corretta"}),";"]}),`
+`,n.jsxs(e.li,{children:["1 punto per una risposta ",n.jsx(e.em,{children:"non data"}),";"]}),`
+`,n.jsxs(e.li,{children:["0 punti per una risposta ",n.jsx(e.em,{children:"sbagliata"}),"."]}),`
+`]}),`
+`,n.jsxs(e.p,{children:["Le domande a ",n.jsx(e.em,{children:"blocchi"}),` richiedono di scrivere un singolo programma a blocchi, che viene valutato su tre diversi livelli.
+Per ciascuna domanda e per ciascun livello, Il punteggio che puoi ottenere è:`]}),`
+`,n.jsxs(e.ul,{children:[`
+`,n.jsxs(e.li,{children:["5 punti se il programma produce la risposta ",n.jsx(e.em,{children:"corretta"}),";"]}),`
+`,n.jsxs(e.li,{children:["0 punti se il programma produce una risposta ",n.jsx(e.em,{children:"sbagliata"}),"."]}),`
+`]}),`
+`,n.jsx(e.p,{children:"Quindi ogni domanda a blocchi può valere fino a 15 punti in totale."})]})}function D(i={}){const{wrapper:e}={...g(),...i.components};return e?n.jsx(e,{...i,children:n.jsx(m,{...i})}):m(i)}const G={title:"Giochi di Fibonacci",description:"Prova di allenamento per la seconda fase dei Giochi di Fibonacci 2023/2024 per le scuole secondarie"};function A(){return n.jsxs(f,{contestLongName:"",contestName:"Giochi di Fibonacci",duration:100,children:[n.jsx(D,{}),n.jsx(z,{})]})}export{A as default,G as metadata};
+//# sourceMappingURL=aaab41a0-page.js.map
--- assets/v/be4c9621-hint.js
+++
@@ -1,3 +0,0 @@
-/*! For licenses information, see LICENSES.txt */
-import{r as o,j as r}from"../_virtual_quizms-routes-d9cb23e2.js";import{C as n,S as i}from"../w/10869bfd-index.js";function u({label:s="suggerimento",children:e}){const t=o.useRef(null);return r.jsxs("div",{className:"w-full flex justify-center my-2",children:[r.jsxs(n,{onClick:()=>t.current?.showModal(),children:["Mostra ",s]}),r.jsx(i,{ref:t,title:s.charAt(0).toUpperCase()+s.slice(1),children:e})]})}export{u as H};
-//# sourceMappingURL=be4c9621-hint.js.map
