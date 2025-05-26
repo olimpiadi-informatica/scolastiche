5cfca2c0eb996ecd455102daee9539d8f747336f
Harniver <Harniver@users.noreply.github.com>
2025-05-26 12:13:20 +0000

    deploy: ffa193d49502115e7e27243a665ed0576ddaf0a4

--- assets/v/6a92962f-hint.js
+++
@@ -1,3 +0,0 @@
-/*! For licenses information, see LICENSES.txt */
-import{r as o,j as r}from"../_virtual_quizms-routes-260c4e05.js";import{C as n,S as i}from"../w/121f6c85-index.js";function u({label:s="suggerimento",children:e}){const t=o.useRef(null);return r.jsxs("div",{className:"w-full flex justify-center my-2",children:[r.jsxs(n,{onClick:()=>t.current?.showModal(),children:["Mostra ",s]}),r.jsx(i,{ref:t,title:s.charAt(0).toUpperCase()+s.slice(1),children:e})]})}export{u as H};
-//# sourceMappingURL=6a92962f-hint.js.map
