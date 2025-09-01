ff92ae5567b6c46fea9d300f04e3bfe35c00ee0c
Harniver <Harniver@users.noreply.github.com>
2025-09-01 14:37:10 +0000

    deploy: 6e2933a13ce05e0364d0e21bde7a6745801d3f33

---
+++ assets/t/1f8e8e3a-page.js
@@ -0,0 +1,20 @@
+/*! For licenses information, see LICENSES.txt */
+import{j as e}from"../_virtual_quizms-routes-3e20caca.js";import{P as x,I as g}from"../u/2e388509-components.js";import{M as C,b,d as P,a as D,c as A,e as M,f as X}from"../o/4ed716d5-question.js";function j(){return{...x(),A11ConnettiStati:X,A4SottraiPotenze:M,P6DistribuisciCarote:A,A21OrdinaConigli:D,A20DueCarote:P,A22PesaLaFrutta:b,P5PercorsoAdOstacoli:C}}function u(n){const i={h2:"h2",...j(),...n.components},{A20DueCarote:s,A21OrdinaConigli:a,A22PesaLaFrutta:c,A4SottraiPotenze:d,Contest:l,P5PercorsoAdOstacoli:p,P6DistribuisciCarote:m,Problem:t,Section:r}=i;return s||o("A20DueCarote"),a||o("A21OrdinaConigli"),c||o("A22PesaLaFrutta"),d||o("A4SottraiPotenze"),l||o("Contest"),p||o("P5PercorsoAdOstacoli"),m||o("P6DistribuisciCarote"),t||o("Problem"),r||o("Section"),e.jsxs(l,{children:[e.jsx(i.h2,{children:"Sezione 1: pensiero logico-algoritmico"}),e.jsxs(r,{children:[e.jsx(t,{points:[5,1,0],id:1,children:e.jsx(s,{})}),e.jsx(t,{points:[5,1,0],id:2,children:e.jsx(a,{})}),e.jsx(t,{points:[5,1,0],id:3,children:e.jsx(c,{})}),e.jsx(t,{points:[5,1,0],id:4,children:e.jsx(d,{})})]}),e.jsx(i.h2,{children:"Sezione 2: procedimenti procedurali"}),e.jsxs(r,{children:[e.jsx(t,{points:[5,1,0],id:5,children:e.jsx(m,{})}),e.jsx(t,{points:[5,1,0],id:6,children:e.jsx(p,{})})]})]})}function S(n={}){const{wrapper:i}={...j(),...n.components};return i?e.jsx(i,{...n,children:e.jsx(u,{...n})}):u(n)}function o(n,i){throw new Error("Expected component `"+n+"` to be defined: you likely forgot to import, pass, or provide it.")}function f(){return{...x()}}function h(n){const i={em:"em",h1:"h1",h2:"h2",li:"li",p:"p",strong:"strong",ul:"ul",...f(),...n.components};return e.jsxs(e.Fragment,{children:[e.jsx(i.h1,{children:"Giochi di Fibonacci 2024/2025"}),`
+`,e.jsx(i.h2,{children:"Fase I - Scuole primarie"}),`
+`,e.jsxs(i.p,{children:["Questa prova contiene ",e.jsx(i.em,{children:"6 problemi"})," da risolvere in ",e.jsx(i.em,{children:"50 minuti"}),", ed è divisa in due parti:"]}),`
+`,e.jsxs(i.ul,{children:[`
+`,e.jsxs(i.li,{children:["quattro problemi di ",e.jsx(i.strong,{children:"pensiero logico-algoritmico"}),", e"]}),`
+`,e.jsxs(i.li,{children:["due problemi di interpretazione di ",e.jsx(i.strong,{children:"procedimenti procedurali"})," come programmi a blocchi."]}),`
+`]}),`
+`,e.jsxs(i.p,{children:[`In entrambe le parti i problemi sono ordinati per difficoltà crescente.
+`,e.jsx(i.strong,{children:"Attento che la difficoltà è soggettiva!"})," Se stai passando tanto tempo cercando di risolvere un problema, prova a passare ad altri problemi e altre categorie!"]}),`
+`,e.jsx(i.h2,{children:"Punteggio"}),`
+`,e.jsxs(i.p,{children:["Ogni problema comprende ",e.jsx(i.em,{children:"due domande"}),`, valutate separatamente.
+La prima domanda è `,e.jsx(i.strong,{children:"sempre più semplice"}),`, e può aiutare a rispondere alla seconda domanda.
+Le domande sono tutte a `,e.jsx(i.strong,{children:"scelta multipla"})," con 5 opzioni, di cui ",e.jsx(i.strong,{children:"solo una"})," è corretta. Il punteggio che puoi ottenere è:"]}),`
+`,e.jsxs(i.ul,{children:[`
+`,e.jsxs(i.li,{children:["5 punti per una risposta ",e.jsx(i.em,{children:"corretta"}),";"]}),`
+`,e.jsxs(i.li,{children:["1 punto per una risposta ",e.jsx(i.em,{children:"non data"}),";"]}),`
+`,e.jsxs(i.li,{children:["0 punti per una risposta ",e.jsx(i.em,{children:"sbagliata"}),"."]}),`
+`]})]})}function $(n={}){const{wrapper:i}={...f(),...n.components};return i?e.jsx(i,{...n,children:e.jsx(h,{...n})}):h(n)}const z={title:"Giochi di Fibonacci",description:"Prima fase dei Giochi di Fibonacci 2024/2025 per le scuole primarie"};function O(){return e.jsxs(g,{contestLongName:"",contestName:"Giochi di Fibonacci",duration:50,children:[e.jsx($,{}),e.jsx(S,{})]})}export{O as default,z as metadata};
+//# sourceMappingURL=1f8e8e3a-page.js.map
