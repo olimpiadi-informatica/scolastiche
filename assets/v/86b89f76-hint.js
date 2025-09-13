62d48a625b25935a9e9f6e8b98cd2fc892235a94
bortoz <bortoz@users.noreply.github.com>
2025-09-13 22:27:44 +0000

    deploy: f8f4ef04065bd2328258a09b8a9354dbc7afa1da

---
+++ assets/v/86b89f76-hint.js
@@ -0,0 +1,3 @@
+/*! For licenses information, see LICENSES.txt */
+import{r as o,j as r}from"../_virtual_quizms-routes-382e58e2.js";import{C as n,S as i}from"../w/fe04458e-index.js";function u({label:s="suggerimento",children:e}){const t=o.useRef(null);return r.jsxs("div",{className:"w-full flex justify-center my-2",children:[r.jsxs(n,{onClick:()=>t.current?.showModal(),children:["Mostra ",s]}),r.jsx(i,{ref:t,title:s.charAt(0).toUpperCase()+s.slice(1),children:e})]})}export{u as H};
+//# sourceMappingURL=86b89f76-hint.js.map
