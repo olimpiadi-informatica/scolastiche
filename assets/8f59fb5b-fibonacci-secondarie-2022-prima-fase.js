adbd23fb8d5acd3b7b489f144e85caf157ae0616
Harniver <Harniver@users.noreply.github.com>
2026-04-11 17:21:24 +0000

    deploy: da88c81454a9bcf2ccfc63d7fc2fa6a6b321ec75

---
+++ assets/8f59fb5b-fibonacci-secondarie-2022-prima-fase.js
@@ -0,0 +1,22 @@
+/*! For licenses information, see LICENSES.txt */
+import{j as e}from"./y/77ae87a1-jsx-runtime.js";import{c as r}from"./l/253b04a2-global.js";import{B as a,p as o}from"./v/e2bfe766-provider.js";import{S as l}from"./y/08223266-components-client.js";function t(){return{...l()}}function s(n){const i={em:"em",h2:"h2",li:"li",p:"p",strong:"strong",ul:"ul",...t(),...n.components};return e.jsxs(e.Fragment,{children:[e.jsxs(i.p,{children:["Questa prova contiene ",e.jsx(i.em,{children:"10 domande"})," ed è pensata per essere risolta in ",e.jsx(i.em,{children:"50 minuti"}),". La prova consiste di quesiti a ",e.jsx(i.strong,{children:"scelta multipla"})," e quesiti a ",e.jsx(i.strong,{children:"domanda aperta numerica"}),", ed è divisa in tre parti:"]}),`
+`,e.jsxs(i.ul,{children:[`
+`,e.jsxs(i.li,{children:["quesiti di ",e.jsx(i.strong,{children:"pensiero logico"}),","]}),`
+`,e.jsxs(i.li,{children:["quesiti di ",e.jsx(i.strong,{children:"pensiero algoritmico"}),", e"]}),`
+`,e.jsxs(i.li,{children:["quesiti di interpretazione di ",e.jsx(i.strong,{children:"procedimenti procedurali"})," come diagrammi di flusso."]}),`
+`]}),`
+`,e.jsxs(i.p,{children:["In ciascuna delle tre parti, i quesiti sono ",e.jsx(i.em,{children:"indicativamente"})," ordinati per difficoltà crescente. ",e.jsx(i.em,{children:"Attenzione"}),": la difficoltà è spesso soggettiva! Se stai passando tanto tempo cercando di risolvere un quesito, considera di dare uno sguardo al successivo, o ai problemi di un’altra categoria!"]}),`
+`,e.jsx(i.h2,{children:"Punteggio"}),`
+`,e.jsxs(i.p,{children:["Tutti i quesiti a ",e.jsx(i.em,{children:"scelta multipla"})," presentano 5 opzioni, di cui ",e.jsx(i.strong,{children:"solo una"})," è corretta. Il punteggio assegnato per tali quesiti è:"]}),`
+`,e.jsxs(i.ul,{children:[`
+`,e.jsxs(i.li,{children:["5 punti per una risposta ",e.jsx(i.em,{children:"corretta"}),";"]}),`
+`,e.jsxs(i.li,{children:["1 punto per una risposta ",e.jsx(i.em,{children:"in bianco"}),";"]}),`
+`,e.jsxs(i.li,{children:["0 punti per una risposta ",e.jsx(i.em,{children:"errata"}),"."]}),`
+`]}),`
+`,e.jsxs(i.p,{children:["Ogni ",e.jsx(i.em,{children:"domanda aperta numerica"})," richiede come risposta un numero intero (eventualmente negativo). Il punteggio assegnato per tali domande è:"]}),`
+`,e.jsxs(i.ul,{children:[`
+`,e.jsxs(i.li,{children:["5 punti per una risposta ",e.jsx(i.em,{children:"corretta"}),";"]}),`
+`,e.jsxs(i.li,{children:["0 punti per una risposta ",e.jsx(i.em,{children:"errata"})," o ",e.jsx(i.em,{children:"in bianco"}),"."]}),`
+`]}),`
+`,e.jsx(i.p,{children:"Attenzione che alcuni quesiti potrebbero contenere più domande, ciascuna valutata separatamente."})]})}function c(n={}){const{wrapper:i}={...t(),...n.components};return i?e.jsx(i,{...n,children:e.jsx(s,{...n})}):s(n)}function d(){return r(e.jsxs(a,{contest:{id:"fibonacci-secondarie-2022-prima-fase",name:"Edizione 2022/23 - Prima fase",longName:"Giochi di Fibonacci 2022/2023 per le scuole secondarie - Prima fase",problemIds:[],userData:[{name:"surname",label:"Cognome",type:"text",pattern:"[\\-'\\s\\p{Alpha}]+",patternMismatch:"Il campo non può contenere numeri o simboli.",patternHelp:"Assicurati che non ci siano numeri o simboli",title:!1},{name:"name",label:"Nome",type:"text",pattern:"[\\-'\\s\\p{Alpha}]+",patternMismatch:"Il campo non può contenere numeri o simboli.",patternHelp:"Assicurati che non ci siano numeri o simboli",title:!1}],matchingRules:[],hasVariants:!0,onlineSettings:{contestWindow:{start:"2000-01-01T00:00:00.000Z",end:"2099-12-31T23:59:59.000Z"},duration:50,allowRestart:!0},hasOffline:!1,allowStudentImport:!1,allowStudentEdit:!1,allowAnswerEdit:!1,allowStudentDelete:!0,scoreVisibility:"finalized",secret:"",entry:"fibonacci-secondarie/2022-prima-fase/contest/contest.mdx",header:"fibonacci-secondarie/2022-prima-fase/header.md",variantIds:["1"],pdfVariantIds:[],shuffleProblems:!1,shuffleAnswers:!0,pdfPerSchool:1},children:[e.jsx(c,{}),e.jsx(o,{})]}))}d();
+//# sourceMappingURL=8f59fb5b-fibonacci-secondarie-2022-prima-fase.js.map
---
+++ assets/921d110d-fibonacci-primarie-2023-prima-fase.js
@@ -0,0 +1,24 @@
+/*! For licenses information, see LICENSES.txt */
+import{j as e}from"./y/77ae87a1-jsx-runtime.js";import{c as s}from"./l/253b04a2-global.js";import{B as a,p as o}from"./v/e2bfe766-provider.js";import{S as l}from"./y/08223266-components-client.js";function r(){return{...l()}}function t(i){const n={em:"em",h2:"h2",li:"li",p:"p",strong:"strong",ul:"ul",...r(),...i.components};return e.jsxs(e.Fragment,{children:[e.jsxs(n.p,{children:["Questa prova contiene ",e.jsx(n.em,{children:"9 domande"})," da risolvere in ",e.jsx(n.em,{children:"50 minuti"}),`.
+Le domande sono a `,e.jsx(n.strong,{children:"scelta multipla"})," o a ",e.jsx(n.strong,{children:"risposta aperta numerica"}),", e sono divise in tre parti:"]}),`
+`,e.jsxs(n.ul,{children:[`
+`,e.jsxs(n.li,{children:["domande di ",e.jsx(n.strong,{children:"pensiero logico"}),","]}),`
+`,e.jsxs(n.li,{children:["domande di ",e.jsx(n.strong,{children:"pensiero algoritmico"}),", e"]}),`
+`,e.jsxs(n.li,{children:["domande di interpretazione di ",e.jsx(n.strong,{children:"procedimenti procedurali"})," come programmi a blocchi."]}),`
+`]}),`
+`,e.jsxs(n.p,{children:[`In tutte le tre parti, le domande sono ordinate per difficoltà crescente.
+`,e.jsx(n.strong,{children:"Attento che la difficoltà è soggettiva!"})," Se stai passando tanto tempo cercando di risolvere una domanda, prova a passare ad altre domande e altre categorie!"]}),`
+`,e.jsx(n.h2,{children:"Punteggio"}),`
+`,e.jsxs(n.p,{children:["Tutte le domande a ",e.jsx(n.em,{children:"scelta multipla"})," hanno 5 opzioni, di cui ",e.jsx(n.strong,{children:"solo una"})," è corretta. Il punteggio che puoi ottenere è:"]}),`
+`,e.jsxs(n.ul,{children:[`
+`,e.jsxs(n.li,{children:["5 punti per una risposta ",e.jsx(n.em,{children:"corretta"}),";"]}),`
+`,e.jsxs(n.li,{children:["1 punto per una risposta ",e.jsx(n.em,{children:"non data"}),";"]}),`
+`,e.jsxs(n.li,{children:["0 punti per una risposta ",e.jsx(n.em,{children:"sbagliata"}),"."]}),`
+`]}),`
+`,e.jsxs(n.p,{children:["Le domande a ",e.jsx(n.em,{children:"risposta aperta"})," hanno come risposta un numero (senza virgola o segno). Il punteggio che puoi ottenere è:"]}),`
+`,e.jsxs(n.ul,{children:[`
+`,e.jsxs(n.li,{children:["5 punti per una risposta ",e.jsx(n.em,{children:"corretta"}),";"]}),`
+`,e.jsxs(n.li,{children:["0 punti per una risposta ",e.jsx(n.em,{children:"sbagliata"})," o ",e.jsx(n.em,{children:"non data"}),"."]}),`
+`]}),`
+`,e.jsx(n.p,{children:"Attenzione che alcune domande potrebbero richiedere più risposte numeriche a richieste diverse, tutte valutate separatamente!"})]})}function c(i={}){const{wrapper:n}={...r(),...i.components};return n?e.jsx(n,{...i,children:e.jsx(t,{...i})}):t(i)}function d(){return s(e.jsxs(a,{contest:{id:"fibonacci-primarie-2023-prima-fase",name:"Edizione 2023/24 - Prima fase",longName:"Giochi di Fibonacci 2023/2024 per le scuole primarie - Prima fase",problemIds:[],userData:[{name:"surname",label:"Cognome",type:"text",pattern:"[\\-'\\s\\p{Alpha}]+",patternMismatch:"Il campo non può contenere numeri o simboli.",patternHelp:"Assicurati che non ci siano numeri o simboli",title:!1},{name:"name",label:"Nome",type:"text",pattern:"[\\-'\\s\\p{Alpha}]+",patternMismatch:"Il campo non può contenere numeri o simboli.",patternHelp:"Assicurati che non ci siano numeri o simboli",title:!1}],matchingRules:[],hasVariants:!0,onlineSettings:{contestWindow:{start:"2000-01-01T00:00:00.000Z",end:"2099-12-31T23:59:59.000Z"},duration:50,allowRestart:!0},hasOffline:!1,allowStudentImport:!1,allowStudentEdit:!1,allowAnswerEdit:!1,allowStudentDelete:!0,scoreVisibility:"finalized",secret:"",entry:"fibonacci-primarie/2023-prima-fase/contest/contest.mdx",header:"fibonacci-primarie/2023-prima-fase/header.md",variantIds:["1","2","3","4","5"],pdfVariantIds:[],shuffleProblems:!1,shuffleAnswers:!0,pdfPerSchool:1},children:[e.jsx(c,{}),e.jsx(o,{})]}))}d();
+//# sourceMappingURL=921d110d-fibonacci-primarie-2023-prima-fase.js.map
